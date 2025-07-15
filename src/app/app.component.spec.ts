c14dcd8ebff61a39f18da8e42499c052c9952689
Guillermo Palacios <nothicc04@gmail.com>
2025-07-14 22:22:18 -0500

    fix: correcting config app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'pruebita' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pruebita');
+    expect(app.title).toEqual('monona');
   });
 
   it('should render title', () => {
